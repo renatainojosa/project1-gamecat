78ffd669bbe31a1b75927ebc11742e8a675947e3
Renata Inojosa <inojosa.renata@gmail.com>
2022-07-23 21:22:59 -0300

    inclusão classe foods

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ backgroundImg.onload = () => {
 
 function clearCanvas() {
   ctx.clearRect(0, 0, 900, 500);
-}
+};
 
 function drawGameBackground() {
   let backImg = new Image();
   backImg.src = "./images/background01.jpg";
   ctx.drawImage(backImg, 0, 0, 900, 500);
-}
+};
 
 document.addEventListener("keydown", (event) => {
   if (event.code === "Space") {
@@ -64,6 +64,51 @@ function updateCanvas() {
 }
 
 class Foods {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+
+    const pizza = new Image();
+    pizza.src = "./images/pizza.png";
+    pizza.onload = () => {
+      this.pizza = pizza;
+    };
+    const burguer = new Image();
+    burguer.src = "./images/burger.png";
+    burguer.onload = () => {
+      this.burguer = burguer;
+    };
+    const iceCream = new Image();
+    iceCream.src = "./images/ice.png";
+    iceCream.onload = () => {
+      this.iceCream = iceCream;
+    };
+    const orange = new Image();
+    orange.src = "./images/orange.png";
+    orange.onload = () => {
+      this.orange = orange;
+    };
+    const frenchFries = new Image();
+    frenchFries.src = "./images/french-fries.png";
+    frenchFries.onload = () => {
+      this.frenchFries = frenchFries;
+    };
+    const ribs = new Image();
+    ribs.src = "./images/spare-ribs.png";
+    ribs.onload = () => {
+      this.ribs = ribs;
+    };
+  }
+  draw() {
+    ctx.drawImage(this.pizza, this.x, this.y, 50, 50);
+    ctx.drawImage(this.burguer, this.x, this.y, 50, 50);
+    ctx.drawImage(this.iceCream, this.x, this.y, 50, 50);
+    ctx.drawImage(this.orange, this.x, this.y, 50, 50);
+    ctx.drawImage(this.frenchFries, this.x, this.y, 50, 50);
+    ctx.drawImage(this.ribs, this.x, this.y, 50, 50);
+  }
+  moveDown() {}
+
   // imagens foods
   // foods de cima para baixo
   // math.random
